ec2ed1d47640f4f8e02c84dfd69025d5356d6d74
abraun20 <allanvobraun@gmail.com>
2020-08-16 18:40:58 -0300

    Modificaçoes nas variaveis do firebase

--- src/inicializadores/init_firebase.js
+++ src/inicializadores/init_firebase.js
@@ -1,14 +1,15 @@
 import admin from 'firebase-admin';
 import dotenv from 'dotenv';
-import { createRequire } from 'module';
 dotenv.config();
-const require = createRequire(import.meta.url);
-
-const serviceAccount = require("../../data/telegrambotbcc-firebase-adminsdk-xcrhy-9448af7119.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL
+  credential: admin.credential.cert({
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    project_id: process.env.FIREBASE_PROJECT_ID
+  }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+
 });
 
 export const db = admin.firestore();
